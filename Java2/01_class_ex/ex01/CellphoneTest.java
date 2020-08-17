0c9e61291bf8b3e29fc53ce26673a6bf148ed931
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-17 19:41:35 +0900

    Update CellphoneTest.java

---
+++ Java2/01_class_ex/ex01/CellphoneTest.java
@@ -0,0 +1,30 @@
+package Ex02;
+
+public class CellphoneTest {
+
+	public static void main(String[] args) {
+		// test
+		Cellphone cellphone1 = new Cellphone();
+		System.out.println(cellphone1.color);
+		System.out.println(cellphone1.maker);
+		System.out.println(cellphone1.price);
+		System.out.println(cellphone1.model);
+		
+		cellphone1.call();
+		cellphone1.camera();
+		
+		Cellphone[] cellphones = new Cellphone[100];
+		for (int i = 0; i < 100; i++) {
+			cellphones[i] = new Cellphone();
+		}
+		
+		//cellphone1.maker = "samsung";
+		//cellphone1.model = "z flip";
+		//System.out.println(cellphone1.maker);
+		//System.out.println(cellphone1.model);
+		
+    cellphone1.setMaker("samsung");
+    cellphone1.getMaker();
+	}
+
+}
