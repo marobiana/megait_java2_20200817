2b0e2c00ff0aa10f9fc6c0739b4070c8ba3f4cbb
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-18 12:42:39 +0900

    Update CellphoneTest.java

--- Java2/01_class_ex/ex01/CellphoneTest.java
+++ Java2/01_class_ex/ex01/CellphoneTest.java
@@ -1,31 +1,29 @@
-package Ex02;
+package ex01;
 
 public class CellphoneTest {
 
 	public static void main(String[] args) {
-		// test
 		Cellphone cellphone1 = new Cellphone();
-		System.out.println(cellphone1.color);
-		System.out.println(cellphone1.maker);
-		System.out.println(cellphone1.price);
-		System.out.println(cellphone1.model);
+//		System.out.println(cellphone1.maker);
+//		System.out.println(cellphone1.model);
+//		System.out.println(cellphone1.color);
 		
 		cellphone1.call();
 		cellphone1.camera();
 		
 		Cellphone[] cellphones = new Cellphone[100];
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < cellphones.length; i++) {
 			cellphones[i] = new Cellphone();
 		}
 		
 		//cellphone1.maker = "samsung";
 		//cellphone1.model = "z flip";
-		//System.out.println(cellphone1.maker);
-		//System.out.println(cellphone1.model);
-		
-    		cellphone1.setMaker("samsung");
-    		String maker = cellphone1.getMaker();
+		String maker = cellphone1.getMaker();
 		System.out.println(maker);
+		cellphone1.setMaker("samsung");
+		
+		String maker1 = cellphone1.getMaker();
+		System.out.println(maker1);
 	}
 
 }
