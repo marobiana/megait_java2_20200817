12466bcabc972df4b9dfd75916ed006ac50f78ea
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-18 14:34:55 +0900

    Update OmrCardTest.java

--- Java2/02_constructor_ex/ex02/OmrCardTest.java
+++ Java2/02_constructor_ex/ex02/OmrCardTest.java
@@ -1,11 +1,13 @@
 package ex01;
 
 public class OmrCardTest {
-
 	public static void main(String[] args) {
 		OmrCard omrCard = new OmrCard();
 		omrCard.setName("신보람");
-		System.out.println("이름: " + omrCard.getName());
+		omrCard.setStudentId(1);
+		omrCard.setAnswer(3, 4, 4, 1, 2);
+		
+		OmrCardReader omrCardReader = new OmrCardReader(3, 4, 4, 5, 1);
+		omrCardReader.printScore(omrCard);
 	}
-
 }
