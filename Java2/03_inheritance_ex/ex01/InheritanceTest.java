8d776a0d93bba2926932e5e4b51398f5ffe6ff2a
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-20 12:48:17 +0900

    Update InheritanceTest.java

---
+++ Java2/03_inheritance_ex/ex01/InheritanceTest.java
@@ -0,0 +1,9 @@
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+//		Person person = new Person("신보람");
+//		person.introduce();
+		
+		Student student = new Student("신보람111", "컴퓨터공학");
+		student.introduce();
+	}
