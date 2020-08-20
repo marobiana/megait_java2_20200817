09afb02f34e1ccd0675a123d6702eaa82a894c0d
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-20 12:50:26 +0900

    Update Pen.java

---
+++ Java2/03_inheritance_quiz/Pen.java
@@ -0,0 +1,12 @@
+package ex03;
+
+public class Pen {
+	private int amount; // 남은 량
+	
+	public int getAmount() { 
+		return amount; 
+	}
+	public void setAmount (int amount) { 
+		this.amount = amount; 
+	}
+}
