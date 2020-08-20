5ef434418b4d769a696340db39fccfd7c971ba78
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-20 12:50:39 +0900

    Update SharpPencil.java

---
+++ Java2/03_inheritance_quiz/SharpPencil.java
@@ -0,0 +1,13 @@
+package ex03;
+
+public class SharpPencil extends Pen {
+	private int width; // 펜의 굵기
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+}
