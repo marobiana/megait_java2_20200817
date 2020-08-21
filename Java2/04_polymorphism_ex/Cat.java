16753b69680c7a4b6d7b35749d8fb014ca79894f
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-21 12:47:58 +0900

    Update Cat.java

---
+++ Java2/04_polymorphism_ex/Cat.java
@@ -0,0 +1,12 @@
+package ex01;
+
+public class Cat extends Animal {
+	@Override
+	public void cry() {
+		System.out.println("야옹");
+	}
+	
+	public void grooming() {
+		System.out.println("그루밍 핥짝핥짝");
+	}
+}
