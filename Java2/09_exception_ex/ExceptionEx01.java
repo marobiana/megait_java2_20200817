142f2df86934ca41a42f89d39f3f84f142d9a982
marobiana <69413030+marobiana@users.noreply.github.com>
2020-08-28 12:56:05 +0900

    Update ExceptionEx01.java

---
+++ Java2/09_exception_ex/ExceptionEx01.java
@@ -0,0 +1,56 @@
+package ex02;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) throws Exception {
+		// 예외처리(Exception) : 예상치 못한 상황을 대처하는 것
+		
+		// 자주 발생하는 예외
+		// 1. NullPointerException => NPE
+		// 2. ArrayIndexOutOfBoundsException 
+		// 3. ClassCastException
+		
+		try {
+			// 1. NPE
+//			String a = null;
+//			if (a.isEmpty()) { // npe 발생 지점
+//				System.out.println("비어있다.");
+//			} else {
+//				System.out.println("비어있지 않다.");
+//			}
+			
+			// 배열 참조 예외
+//			int[] arr = new int[3];
+//			for (int i = 0; i <= arr.length; i++) {
+//				arr[i] = i;
+//				System.out.println(arr[i]);
+//			}
+			
+			// 캐스팅 예외
+			Object n = 10;
+			System.out.println(n);
+			
+		} catch (NullPointerException npe) {
+			System.out.println("NPE가 발생했다.");
+			npe.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열의 범위를 벗어났다.");
+		} catch (ClassCastException e) {
+			System.out.println("캐스팅 예외가 발생했다.");
+		} finally {
+			System.out.println("finally");
+		}
+		
+		System.out.println("수행 완료");
+		
+		a();
+	}
+
+	public static void a() throws Exception {
+		b();
+	}
+	
+	public static void b() throws Exception {
+		throw new Exception();
+	}
+}
