c238b409c37997bd66537427b21045f536c2035a
marobiana <69413030+marobiana@users.noreply.github.com>
2021-05-10 14:58:13 +0900

    Update Stack.java

--- Java2/08_collection_quiz/ex03/Stack.java
+++ Java2/08_collection_quiz/ex03/Stack.java
@@ -5,12 +5,19 @@ import java.util.List;
 
 public class Stack {
 	private List<String> stack = new ArrayList<>();
+	private int top = -1;
 	
 	public void push(String value) {
 		this.stack.add(value);
+		this.top++;
 	}
 	
 	public String pop() {
 		return this.stack.remove(this.stack.size() - 1);
+		this.top--;
+	}
+	
+	public String peek() {
+		this.stack.get(this.top);	
 	}
 }
